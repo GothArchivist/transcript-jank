ae3ccf74c521ffe54addbc8ec4cab21e5ca98673
Christy <christy.tomecek@yale.edu>
2026-05-22 10:24:28 -0400

    Update check_sensitive_language.py
    Updated script to have the regex word boundary added by the script rather than having the user add the word boundary to the lexicon.

--- sensitive-language-alert/check_sensitive_language.py
+++ sensitive-language-alert/check_sensitive_language.py
@@ -8,7 +8,7 @@ import os
 import pandas as pd
 
 def lexicon_input(): #This is where you load your lexicon of choice in CSV format. 
-    file = open("path/to/lexicon/csv", 'r', encoding='utf-8') #Use the full file path, including the drive (like C:/) within quotation marks.
+    file = open("C:/Users/ct524/Documents/Transcripts/TranscriptReview/sample_lexicon_real2.csv", 'r', encoding='utf-8') #Use the full file path, including the drive (like C:/) within quotation marks.
     csvin = csv.reader(file)
     next(csvin, None)
     return csvin
@@ -18,19 +18,21 @@ def match():
     lexicon = lexicon_input() #gets the lexicon referenced in the function before
     for row in lexicon:
         term = row[0] #terms should be in the first column
-        transcriptDirectory = r"/path/to/directory" #Filepath to folder that the transcripts are stored in. This should be different from where the lexicon and report are/will be stored
+        pattern = r'\b' + re.escape(term) + r'\b'
+        t = re.compile(pattern, re.I)
+        print(t)
+        transcriptDirectory = r"C:/Users/ct524/Documents/Transcripts/ReneeHertha" #Filepath to folder that the transcripts are stored in. This should be different from where the lexicon and report are/will be stored
         for e in os.scandir(transcriptDirectory):
             identify_file = e
             af = os.path.basename(identify_file) #This captures only the filename for the report for brevity's sakes
-            transcript = open(e.path, "r")
+            transcript = open(e.path, "r", encoding='utf-8')
             tr = transcript.read()
-            for match in re.finditer(term, tr, re.I):
-                #print(match.group(), "start index", match.start(), "End index", match.end()) #includes the terms and the index position so you can find it in the document
+            for match in re.finditer(t, tr):
                 alert_file = {'filename': [af], 'alert':[match]}
                 df = pd.DataFrame(alert_file)
                 df_list.append(df)
                 result = pd.concat(df_list)
-                result.to_csv('/path/to/csv', encoding="utf-8") #put in the full filepath. The file does not need to already exist
+                result.to_csv('C:/Users/ct524/Documents/Transcripts/ReneeHertha/alert_2026-05-22_3.csv', encoding="utf-8") #put in the full filepath. The directory needs to already exist.
                 
 match()
 
