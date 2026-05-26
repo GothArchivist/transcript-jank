284f9770616ca23e57f27d7de431904d012fda35
Christy <christy.tomecek@yale.edu>
2026-05-26 16:54:00 -0400

    Created reorderdocx and cleaned up readmes
    Created a new sub-repository for reorderdocx; simplified the main readme and created separate readmes with detailed instructions for each sub-repository.

---
+++ reorderdocx/reorderColumns.py
@@ -0,0 +1,19 @@
+# created for reordering columns in .tsv files. This is part of a workflow to fix transcript formatting in .docx documents. If you're wondering where .tsv files come into this...it's a long story.
+
+import os
+import pandas as pd
+
+directory = '/path/to/folder/with/files' #insert filepath
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f):  # checking if it is a file
+        if f.endswith('.txt'): #I left the extension on these as .txt, but you could do .tsv as well. Or even make this work for .csv!
+            print(f) #Only have this here in case you want to check things.
+            df = pd.read_table(f, sep='\t') #if you want to use a csv, leave off the sep='\t'. 
+            print(df) #Same deal with checking things
+            columnsTitles = ['timestamp', 'speaker', 'text'] #I'm using the names of the columns in the original document but in the order I want them to be in here. 
+            df2 = df.reindex(columns=columnsTitles) #does the reorder
+            print(df2) #same deal with checking things
+            nd = '' #filepath of wherever the new documents should live
+            nf = os.path.join(nd, filename)
+            df2.to_csv(nf, sep='\t', index=False) #Despite the to_csv, you are still saving this as a text file/.tsv the way this script is written. If you actually want to save this as a .csv, remove the sep='\t' part
