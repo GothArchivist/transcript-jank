9bf13ed443de8cf62058126523c505250c2895f3
Christy <christy.tomecek@yale.edu>
2026-07-14 11:16:03 -0400

    Added ability to select file format

--- sensitive-language-alert/check_sensitive_language_with_lines.py
+++ sensitive-language-alert/check_sensitive_language_with_lines.py
@@ -22,21 +22,22 @@ def match():
         t = re.compile(r'\b' + term + r'\b', re.I) #finds the term as standalone words, not case-sensitive
         transcriptDirectory = r"/path/to/file" #Filepath to folder that the transcripts are stored in. This should be different from where the lexicon and report are/will be stored
         for e in os.scandir(transcriptDirectory):
-            identify_file = e
-            af = os.path.basename(identify_file) #This captures only the filename for the report for brevity's sakes
-            transcript = open(e.path, "r")
-            tr = transcript.readlines() #This line and the next sets up the line numbering
-            for line_number, line in enumerate(tr, start=1):
-                found_match = re.findall(t, line)
-                if found_match:
-                    match_list.extend([(af, match, line_number) for match in found_match]) #adds the file name, the alert, and the line number to the list.
-                else:
-                    continue
-                with open('/path/to/file', 'w', encoding='utf-8', newline='') as f: #output file for alerts, also should be stored separately from the lexicon
-                    writer = csv.writer(f)
-                    header = ["file", "alert", "line_number"] #header fields for the CSV output
-                    writer.writerow(header)
-                    writer.writerows(match_list) #Actual output
+            if e.path.endswith(format):
+                identify_file = e
+                af = os.path.basename(identify_file) #This captures only the filename for the report for brevity's sakes
+                transcript = open(e.path, "r", encoding='utf-8')
+                tr = transcript.readlines() #This line and the next sets up the line numbering
+                for line_number, line in enumerate(tr, start=1):
+                    found_match = re.findall(t, line)
+                    if found_match:
+                        match_list.extend([(af, match, line_number) for match in found_match]) #adds the file name, the alert, and the line number to the list.
+                    else:
+                        continue
+                    with open('/path/to/file', 'w', encoding='utf-8', newline='') as f: #output file for alerts, also should be stored separately from the folder with the lexicon
+                        writer = csv.writer(f)
+                        header = ["file", "alert", "line_number"] #header fields for the CSV output
+                        writer.writerow(header)
+                        writer.writerows(match_list) #Actual output
                   
                 
 match()
