591291562323ba6550b590f0f6badb63448c1a25
Christy, Metadata Janktress <christy.tomecek@yale.edu>
2026-07-20 11:21:18 -0400

    Update check_sensitive_language_vtt_srt_regex.py

--- sensitive-language-alert/check_sensitive_language_vtt_srt_regex.py
+++ sensitive-language-alert/check_sensitive_language_vtt_srt_regex.py
@@ -34,11 +34,11 @@ def match():
                     p = re.findall(r'\b'+ term + r'\b', c, re.I)
                     if p:
                         alert_file = {'filename': [af1], 'time-in': [caption.start], 'time-out': [caption.end], 'alert': [caption.text]} #sets up the dataframe/CSV for the report
-                        print(alert_file)
+                        #print(alert_file) #when not commented out, it will display the matches in the terminal if you like instant gratification. 
                         df1 = pd.DataFrame(alert_file) #creates said dataframe
                         match_list.append(df1) #adds the stuff in the match list
                         result = pd.concat(match_list)
-                        result.to_csv(filepath, encoding="utf-8") 
+                        result.to_csv(filepath, encoding="utf-8") #creates the CSV report 
             elif e.path.endswith('.srt'):
                 identify_file = e
                 af2 = os.path.basename(identify_file) #This captures only the filename for the report for brevity's sake
@@ -50,11 +50,11 @@ def match():
                         updated_start = srt.timedelta_to_srt_timestamp(sub.start)
                         updated_end = srt.timedelta_to_srt_timestamp(sub.end)
                         alert_file = {'filename': [af2], 'time-in': [updated_start], 'time-out': [updated_end], 'alert': [sub.content]} #sets up the dataframe/CSV for the report
-                        print(alert_file)
+                        #print(alert_file) #when not commented out, it will display the matches in the terminal if you like instant gratification.
                         df2 = pd.DataFrame(alert_file) #creates said dataframe
                         match_list.append(df2) #adds the stuff in the match list
                         result = pd.concat(match_list)
-                        result.to_csv(filepath, encoding="utf-8") 
+                        result.to_csv(filepath, encoding="utf-8") #creates the CSV report
             else:
                 continue
 
